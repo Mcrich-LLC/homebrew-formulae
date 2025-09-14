a1e9a5edf4f30e5c97d7e5cf89a08b3ef19d4eaf
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2025-09-14 16:20:59 -0700

    Update videopaper.rb

--- Casks/videopaper.rb
+++ Casks/videopaper.rb
@@ -2,16 +2,16 @@ cask "videopaper" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/Mcrich23/VideoPaper/releases/latest/download/VideoPaper.zip"
+  url "https://github.com/Mcrich-LLC/VideoPaper/releases/latest/download/VideoPaper.zip"
   name "VideoPaper"
   desc "Set videos as your wallpaper"
-  homepage "https://github.com/Mcrich23/VideoPaper"
+  homepage "https://github.com/Mcrich-LLC/VideoPaper"
 
   app "VideoPaper.app"
 
   zap trash: [
-    "~/Library/Preferences/com.mcrich23.VideoPaper.plist",
-    "~/Library/Saved Application State/com.mcrich23.VideoPaper.savedState",
+    "~/Library/Preferences/com.mcrich.VideoPaper.plist",
+    "~/Library/Saved Application State/com.mcrich.VideoPaper.savedState",
     "~/Library/Application Support/VideoPaper",
   ]
 end
